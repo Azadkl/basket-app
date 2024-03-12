6e2d31bcace0cc75f8a73e19518a0f3fccc6be3d
Azad Köl <kolazad47@gmail.com>
2024-03-12 13:44:49 +0300

    ilk kısım yapıldı

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "@mantine/core/styles.css";
+import {
+  Container,
+  SimpleGrid,
+  List,
+  ThemeIcon,
+  rem,
+  ListItem,
+  Input,
+} from "@mantine/core";
+import { IconCircleCheck } from "@tabler/icons-react";
+import Card from "./components/Card";
+import { useState } from "react";
+
+const storeItems = [
+  {
+    name: "Akıllı Saat",
+    src:"akillisaat",
+    price: 30,
+  },
+  {
+    name: "Spor Ayakkabı",
+    src:"ayakkabi",
+    price: 20,
+  },
+  {
+    name: "Fotoğraf Makinası",
+    src:"kamera",
+    price: 10,
+  },
+  {
+    name: "Kulaklık",
+    src:"kulaklik",
+    price: 20,
+  },
+  {
+    name: "Oyuncak Araba",
+    src:"oyuncakaraba",
+    price: 20,
+  },
+  {
+    name: "Kol Saati",
+    src:"saat",
+    price: 20,
+  },
+
+];
 
 function App() {
+  let [basketItems, setBasketItems] = useState([]);
+  let [searchValue,setSearchValue]=useState("");
+  let filteredItems = basketItems.filter((item)=> item.name.toLowerCase().indexOf(searchValue.toLowerCase())>=0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <SimpleGrid cols={3} className="Store">
+        {storeItems.map(({ name ,src}) => {
+          return (
+            <Card
+              key={name}
+              name={name}
+              src={src}
+              onAdd={() => setBasketItems([...basketItems, { name: name }])}
+            ></Card>
+          );
+        })}
+      </SimpleGrid>
+      <Input.Wrapper label="Arama" className="List">
+      <Input onChange ={(e)=>setSearchValue(e.target.value)} />
+    </Input.Wrapper>
+      <List
+        className="List"
+        spacing="xs"
+        size="sm"
+        center
+        icon={
+          <ThemeIcon color="teal" size={24} radius="xl">
+            <IconCircleCheck size={16} />
+          </ThemeIcon>
+        }
+      >
+        {filteredItems.map(({ name },index) => (
+          <ListItem key={index}>{name}</ListItem>
+        ))}
+      </List>
+    </Container>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,30 @@
+import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
+
+const CardComponent = ({ name, onAdd, src }) => {
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Card.Section>
+        <Image
+          src={"/assets/image/" + src + ".jpg"}
+          height={160}
+          alt="Norway"
+        />
+      </Card.Section>
+
+      <Group justify="space-between" mt="md" mb="xs">
+        <Text fw={500}>{name}</Text>
+        <Badge color="pink">On Sale</Badge>
+      </Group>
+
+      <Text size="sm" c="dimmed">
+        With Fjord Tours you can explore more of the magical fjord landscapes
+        with tours and activities on and around the fjords of Norway
+      </Text>
+
+      <Button color="blue" fullWidth mt="md" radius="md" onClick={onAdd}>
+        Ekle
+      </Button>
+    </Card>
+  );
+};
+export default CardComponent;
